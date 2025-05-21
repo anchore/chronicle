65563937b437bd99327415b8b31a73ead7517150
Keith Zantow <kzantow@gmail.com>
2025-05-21 12:23:45 -0400

    WIP
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- make/main.go
+++ make/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"path/filepath"
+
 	. "github.com/anchore/go-make"
 	"github.com/anchore/go-make/tasks/golint"
 	"github.com/anchore/go-make/tasks/gotest"
@@ -11,48 +13,8 @@ func main() {
 	Makefile(
 		RollupTask("default", "run all validations", "static-analysis", "test"),
 		golint.Tasks(),
-		release.Tasks(),
-		fixturesFingerprintTask,
-		fixturesTask,
-		gotest.Test("unit", gotest.WithDependencies(fixturesTask.Name)),
+		release.Tasks().DependsOn(fixturesTasks.Name),
+		gotest.FixturesTasks(),
+		gotest.Test("unit", gotest.WithDependencies(fixturesTasks.Name)).DependsOn(gotest.FixturesTas),
 	)
-}
-
-var fixturesTask = Task{
-	Name: "fixtures",
-	Desc: "build test fixtures",
-	Run: func() {
-		InDir("internal/git/test-fixtures", func() {
-			Run("make")
-		})
-
-		InDir("chronicle/release/releasers/github/test-fixtures", func() {
-			Run("make")
-		})
-	},
-	Tasks: []Task{
-		{
-			Name:   "fixtures:clean",
-			Desc:   "clean internal git test fixture caches",
-			Labels: All("clean"),
-			Run: func() {
-				InDir("internal/git/test-fixtures", func() {
-					Run("make clean")
-				})
-
-				InDir("chronicle/release/releasers/github/test-fixtures", func() {
-					Run("make clean")
-				})
-			},
-		},
-	},
-}
-
-var fixturesFingerprintTask = Task{
-	Name:  "fixtures-fingerprint",
-	Desc:  "get test fixtures cache fingerprint",
-	Quiet: true,
-	Run: func() {
-		Log(FingerprintGlobs("internal/git/test-fixtures/*.sh"))
-	},
 }
