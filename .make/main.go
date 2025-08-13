8412bab973eb9bddf0aec5470eac32120f871d38
Keith Zantow <kzantow@gmail.com>
2025-08-13 14:59:02 -0400

    chore: address pr feedback
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- .make/main.go
+++ .make/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	. "github.com/anchore/go-make"
 	"github.com/anchore/go-make/tasks/golint"
+	"github.com/anchore/go-make/tasks/goreleaser"
 	"github.com/anchore/go-make/tasks/gotest"
 	"github.com/anchore/go-make/tasks/release"
 )
@@ -11,7 +12,8 @@ func main() {
 	Makefile(
 		golint.Tasks(),
 		release.Tasks(),
+		goreleaser.Tasks(),
 		gotest.Tasks(),
-		gotest.FixtureTasks().RunOn("release"),
+		gotest.FixtureTasks().RunOn("ci-release"),
 	)
 }
